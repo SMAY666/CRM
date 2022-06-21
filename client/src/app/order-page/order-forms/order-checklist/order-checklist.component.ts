06d19cbe9a1beb68f91a75ee4077e246e8cebe49
SMAY <sazonov_15.03@mail.ru>
2022-06-21 20:03:22 +0700

    CRM

---
+++ client/src/app/order-page/order-forms/order-checklist/order-checklist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-checklist',
+  templateUrl: './order-checklist.component.html',
+  styleUrls: ['./order-checklist.component.css']
+})
+export class OrderChecklistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
