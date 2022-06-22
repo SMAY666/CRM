de604c7d26e9c88c9658606cfa7dec3e5c8cdd02
SMAY <sazonov_15.03@mail.ru>
2022-06-23 00:07:00 +0700

    CRM

--- client/src/app/order-page/order-forms/order-checklist-form/order-checklist.component.ts
+++ client/src/app/order-page/order-forms/order-checklist-form/order-checklist.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {OrderPageComponent} from '../../order-page.component';
 
+@Inject({
+
+})
 @Component({
   selector: 'app-order-checklist-form',
   templateUrl: './order-checklist.component.html',
   styleUrls: ['./order-checklist.component.css']
 })
 export class OrderChecklistComponent implements OnInit {
+    forms: FormGroup[] = []
+    selectedPos = OrderPageComponent.orderPositions
+    constructor() { }
 
-  constructor() { }
+    ngOnInit(): void {
 
-  ngOnInit(): void {
-  }
-
+        for (let i = 0; i < this.selectedPos.length; i++) {
+            this.forms.push(new FormGroup({
+                number: new FormControl(1, [Validators.required, Validators.min(1), Validators.pattern("^[0-9]*[.,]?")])
+            }))
+        }
+    }
 }
